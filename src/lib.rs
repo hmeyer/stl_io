0bfd081fb2fd3e6a1e89441e4008ff1db8455d08
Henning Meyer <tutmann@gmail.com>
2018-04-09 11:52:23 +0200

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! ```stl_io``` is crate for reading and writing [STL (STereoLithography)](https://en.wikipedia.org/wiki/STL_(file_format)) files.
+//! ```stl_io``` is a crate for reading and writing [STL (STereoLithography)](https://en.wikipedia.org/wiki/STL_(file_format)) files.
 //! It can read both, binary and ascii STL in a safe manner.
 //! Writing is limited to binary STL, which is more compact anyway.
 //! # Examples
