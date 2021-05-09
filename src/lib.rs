6ef9580a8034a11504a4ba9efd30f1968e1f2a21
casper <casperlamboo@gmail.com>
2021-05-09 11:26:32 +0200

    Use functions in favour of macros
    as discussed in #11

--- src/lib.rs
+++ src/lib.rs
@@ -521,31 +521,23 @@ impl<'a> AsciiStlReader<'a> {
 }
 
 fn tri_area(a: Vertex, b: Vertex, c: Vertex) -> f32 {
-    macro_rules! cross {
-        ($a:expr, $b:expr) => {
-            {
-                let x = $a[1] * $b[2] - $a[2] * $b[1];
-                let y = $a[2] * $b[0] - $a[0] * $b[2];
-                let z = $a[0] * $b[1] - $a[1] * $b[0];
-                [x, y, z]
-            }
-        };
+    fn cross(a: Vertex, b: Vertex) -> Vertex {
+        let x = a[1] * b[2] - a[2] * b[1];
+        let y = a[2] * b[0] - a[0] * b[2];
+        let z = a[0] * b[1] - a[1] * b[0];
+        Vertex::new([x, y, z])
     }
-    macro_rules! sub {
-        ($a:expr, $b:expr) => {
-            {
-                let x = $a[0] - $b[0];
-                let y = $a[1] - $b[1];
-                let z = $a[2] - $b[2];
-                [x, y, z]
-            }
-        };
+    fn sub(a: Vertex, b: Vertex) -> Vertex {
+        let x = a[0] - b[0];
+        let y = a[1] - b[1];
+        let z = a[2] - b[2];
+        Vertex::new([x, y, z])
     }
-    macro_rules! length {
-        ($v:expr) => { ($v[0] * $v[0] + $v[1] * $v[1] + $v[2] * $v[2]).sqrt() };
+    fn length(v: Vertex) -> f32 {
+        (v[0] * v[0] + v[1] * v[1] + v[2] * v[2]).sqrt()
     }
 
-    length!(cross!(sub!(c, b), sub!(a, b))) * 0.5
+    length(cross(sub(c, b), sub(a, b))) * 0.5
 }
 
 #[cfg(test)]
